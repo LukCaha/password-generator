4f60c28ef3a6e3d1cb68e9be04216fcb4c9c0aa1
Lukáš Caha <lukas.caha@gmail.com>
2016-08-24 00:46:25 +0200

    Update password-generator.php

--- password-generator.php
+++ password-generator.php
@@ -14,7 +14,6 @@
 	  text-align: center;
 	  color: rgba(0, 102, 153, 1);
   }
-
   pre {
 	  text-align: center;
 	  font-size: 2em;
@@ -26,7 +25,6 @@
 	  margin: 0 auto;
 	  font-size: 1.5em;
   }
-
   -->
   </style>
 </head>
@@ -34,11 +32,10 @@
 <body>
 <h1>Generátor hesel</h1>
 <pre><?php
-
 $chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789\|/[]{}<>()#$&@%-+=_!?.:,;AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 for($i=0;$i<12;$i++){
-	echo htmlentities(substr($chars, rand(0,strlen($chars)-1),1));
+	echo htmlentities(substr($chars, rand(0, strlen($chars)-1), 1));
 }
 ?></pre>
 <input name="refresh" type="button" value="Chci jiné" onclick="window.location.reload();">
