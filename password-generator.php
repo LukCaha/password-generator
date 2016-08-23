aebdad5e21b2f4f5eeed4fb772e519bacf46beaf
Lukáš Caha <lukas.caha@gmail.com>
2016-08-24 00:44:05 +0200

    Add files via upload

---
+++ password-generator.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>Generátor hesel</title>
+  <meta charset="utf-8">
+  <style type="text/css">
+  <!--
+  body {
+	  font-family: Verdana, Geneva, sans-serif;
+  }
+  h1 {
+	  text-align: center;
+	  color: rgba(0, 102, 153, 1);
+  }
+
+  pre {
+	  text-align: center;
+	  font-size: 2em;
+	  font-weight: bold;
+	  letter-spacing: .05em;
+  }
+  input {
+	  display: block;
+	  margin: 0 auto;
+	  font-size: 1.5em;
+  }
+
+  -->
+  </style>
+</head>
+
+<body>
+<h1>Generátor hesel</h1>
+<pre><?php
+
+$chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789\|/[]{}<>()#$&@%-+=_!?.:,;AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+for($i=0;$i<12;$i++){
+	echo htmlentities(substr($chars, rand(0,strlen($chars)-1),1));
+}
+?></pre>
+<input name="refresh" type="button" value="Chci jiné" onclick="window.location.reload();">
+</body>
+
+</html>
